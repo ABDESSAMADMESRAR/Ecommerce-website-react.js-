2a2c05ac0fed8725d406cd47109930aea0d7691a
ABDESSAMAD MESRAR <130689222+ABDESSAMADMESRAR@users.noreply.github.com>
2025-01-08 17:35:15 +0100

    e

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import './App.css'
-
+import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Shop from './components/Shop';
-import Shoppingcart from './components/Shoppingcart';
-import Header from './components/Header';
-import Contactus from './components/Contactus';
-import Shopinfo from './components/Shopinfo';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
 
+// Lazy loading the components
+const Home = lazy(() => import('./components/Home'));
+const About = lazy(() => import('./components/About'));
+const Shop = lazy(() => import('./components/Shop'));
+const Shoppingcart = lazy(() => import('./components/Shoppingcart'));
+const Header = lazy(() => import('./components/Header'));
+const Contactus = lazy(() => import('./components/Contactus'));
+const Shopinfo = lazy(() => import('./components/Shopinfo'));
 
 function App() {
-
   return (
     <>
-<BrowserRouter>
-<Header></Header>
-<Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Shop" element={<Shop />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact-us" element={<Contactus />} />
-          <Route path="/ShoppingCart" element={<Shoppingcart/>} />
-          <Route path="/:idsh" element={<Shopinfo />} />
-        </Routes>
-</BrowserRouter> 
+      <BrowserRouter>
+        <Suspense fallback={<div className="loading-screen">Loading...</div>}>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Shop" element={<Shop />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Contact-us" element={<Contactus />} />
+            <Route path="/ShoppingCart" element={<Shoppingcart />} />
+            <Route path="/:idsh" element={<Shopinfo />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
